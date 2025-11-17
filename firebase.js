dfad3b40236fcc05563622dd38f4112b32dfc556
Niska Pillay <130572748+ST10166813@users.noreply.github.com>
2025-11-17 21:18:37 +0200

    Fixed once again

--- firebase.js
+++ firebase.js
@@ -1,9 +1,13 @@
 const admin = require("firebase-admin");
 
+// Parse JSON from env
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON);
 
+// Replace literal \n with actual newlines
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
