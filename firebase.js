22730a60abc219b8599923ecb1097bdd4a473cd3
Niska Pillay <130572748+ST10166813@users.noreply.github.com>
2025-11-17 21:10:57 +0200

    fixd

--- firebase.js
+++ firebase.js
@@ -1,12 +1,9 @@
 const admin = require("firebase-admin");
 
-// Parse the JSON from the environment variable
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-console.log("✅ Firebase Admin initialized successfully");
 
 module.exports = admin;
